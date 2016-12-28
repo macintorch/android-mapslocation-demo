dc95d559ec58741acaebf8b35364e2b368a9a35b
Ainor Syahrizal <macintorch@gmail.com>
2016-12-28 15:13:11 +0800

    get user location address

--- app/src/main/java/ainor/com/my/mapandlocationdemo/MapsActivity.java
+++ app/src/main/java/ainor/com/my/mapandlocationdemo/MapsActivity.java
@@ -4,6 +4,8 @@ import android.*;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -22,6 +24,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -88,6 +94,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 mMap.addMarker(new MarkerOptions().position(userLocation).title("User Location"));
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
 
+                // get user address
+
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+                try {
+                    List<Address> listAddresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(), 1);
+
+                    if (listAddresses != null && listAddresses.size() > 0) {
+                        Log.i("PlaceInfo", listAddresses.get(0).toString());
+                    }
+                } catch (IOException e) {
+
+                    e.printStackTrace();
+                }
+
             }
 
             @Override
@@ -125,7 +146,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
             }
         }
-
 
     }
 
